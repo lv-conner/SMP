c1f037b7a43a68425ddf79c50382d05c360d70b1
lvmh <lvmh123456@gmail.com>
2018-09-03 20:08:37 +0800

    添加Guid生成方法

---
+++ SMP.ApplicationContext/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMP.ApplicationContext
+{
+    public class ApplicationUser:EntityBase
+    {
+        public string Name { get; protected set; }
+    }
+}
---
+++ SMP.ApplicationContext/Entitybase.cs
@@ -0,0 +1,16 @@
+﻿using SLP.Common.GuidGenerator;
+using System;
+
+namespace SMP.ApplicationContext
+{
+    public abstract class EntityBase
+    {
+        public EntityBase()
+        {
+            Id = SequentialGuid.Create(SequentialGuidType.SequentialAtEnd);
+        }
+        public Guid Id { get; protected set; }
+        public DateTime? CreateDate { get; protected set; }
+        public DateTime? UpdateDate { get; protected set; }
+    }
+}
